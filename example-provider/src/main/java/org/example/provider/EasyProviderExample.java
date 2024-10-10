c1a64dbc0a603aaabbea9783854b9d6d1ed9bf5c
fero <877954032@qq.com>
2024-10-10 17:16:44 +0800

    项目初始化

--- example-provider/src/main/java/org/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/org/example/provider/EasyProviderExample.java
@@ -1,10 +1,21 @@
 package org.example.provider;
 
+import org.example.common.service.UserService;
+import org.example.registry.LocalRegistry;
+import org.example.server.HttpServer;
+import org.example.server.VertxHttpServer;
+
 /**
  * 简易服务提供者示例
  */
 public class EasyProviderExample {
+
     public static void main(String[] args) {
-        //提供服务
+        //注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
+        //提供web服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 }
